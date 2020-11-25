bdf1d71943257f61b42042c8f8cd6166ce5af5e5
karelkuijpers <karelkuijpers@gmail.com>
2020-11-25 15:12:00 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,10 +24,10 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder'     => 0,
     'createDirs'       => '',
     'clearCacheOnLoad' => 1,
-    'version'          => '1.0.0',
+    'version'          => '1.1.0',
     'constraints'      => [
         'depends' => [
-            'typo3' => '8.4.0-9.9.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
